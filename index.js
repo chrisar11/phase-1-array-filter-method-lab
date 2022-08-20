95ceae8900d8283f9fdf7d32ea65b03c00edb230
Christopher Sarmiento <chrisar11@gmail.com>
2022-08-20 14:08:29 -0700

    commit tree 702974de91db2017d95e3092342915d825bb04d9

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+// * `findMatching`- This function takes an array of drivers' names and a `string`
+// as arguments, and returns the matching list of drivers. The function should be
+// case insensitive.
+
+function findMatching(names, string) {
+    return names.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase()
+    );
+}
